686a28d747017cbbf33438ebbe72d52a2a3a2bbd
PPWangyc <ppwangyc@gmail.com>
2024-09-19 22:07:47 -0400

    add

--- script/ppwang/train.sh
+++ script/ppwang/train.sh
@@ -28,7 +28,6 @@ if [ $model_mode = "mm" ]; then
                                     --mixed_training \
                                     --num_sessions $num_sessions \
                                     --dummy_size $dummy_size \
-                                    --dummy_load \
                                     --model_mode $model_mode 
 elif [ $model_mode = "encoding" ] || [ $model_mode = "decoding" ];
 then
