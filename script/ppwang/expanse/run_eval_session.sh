b909a3363a761a28118422a558a3e0aeb446b49c
PPWangyc <ppwangyc@gmail.com>
2024-09-25 22:29:07 -0700

    add scripts

---
+++ script/ppwang/expanse/run_eval_session.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+num_sessions=${1}
+model_mode=${2} # mm, decoding, encoding
+train_mode=${3} # train/finetune
+while IFS= read -r line
+do
+    echo "Eval on ses eid: $line"
+    sbatch eval.sh $num_sessions $line $model_mode $train_mode 0.1
+    echo "Command: sbatch eval.sh $num_sessions $line $model_mode $train_mode 0.1"
+done < "../../../data/test_eids.txt"
