a4aff5d6f2aeb86af4e4bf7776f0fabf3e7af3f9
Yizi Zhang <yzhang39@login01.expanse.sdsc.edu>
2024-10-30 21:13:55 -0700

    fix

--- script/yizi/train.sh
+++ script/yizi/train.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-#SBATCH --account=bcxj-delta-cpu
-#SBATCH --partition=cpu
+#SBATCH --account=col169
+#SBATCH --partition=gpu-shared
 #SBATCH --job-name="mm"
 #SBATCH --output="mm.%j.out"
 #SBATCH -N 1
 #SBACTH --array=0
-#SBATCH -c 1
+#SBATCH -c 8
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem 100000
-#SBATCH -t 0-01
+#SBATCH --mem 150000
+#SBATCH --gpus=1
+#SBATCH -t 0-16
 #SBATCH --export=ALL
 
+module load gpu
+module load slurm
 . ~/.bashrc
+
 num_sessions=${1}
 eid=${2}
 model_mode=${3}
