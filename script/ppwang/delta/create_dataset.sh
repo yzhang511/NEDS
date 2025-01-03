03a46b0e06eb35df221b77f030ba9fd27c595184
PPWangyc <ppwangyc@gmial.com>
2025-01-03 17:13:26 -0600

    fix env and add delta scripts

---
+++ script/ppwang/delta/create_dataset.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#!/bin/bash
+#SBATCH -A bcxj-delta-cpu 
+#SBATCH --job-name="create_data"
+#SBATCH --output="create_data.%j.out"
+#SBATCH --partition=cpu
+#SBATCH -c 4
+#SBATCH --mem 100000
+#SBATCH -t 0-01
+#SBATCH --export=ALL
+
+
+. ~/.bashrc
+eid=${1}
+model_mode=mm
+dummy_size=0
+mask_ratio=0.1
+echo $TMPDIR
+conda activate ibl-mm
+
+# get user name
+user_name=$(whoami)
+cd ../../..
+python src/create_dataset.py --eid $eid \
+                                --base_path ./\
+                                --mask_ratio $mask_ratio \
+                                --mixed_training \
+                                --num_sessions 1 \
+                                --dummy_size $dummy_size \
+                                --model_mode $model_mode \
+                                --data_path /scratch/bdtg/${user_name}/datasets/
+
+cd script/ppwang/delta
+
---
+++ script/ppwang/delta/prepare_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -A bcxj-delta-cpu 
+#SBATCH --job-name="prep_data"
+#SBATCH --output="prep_data.%j.out"
+#SBATCH --partition=cpu
+#SBATCH -c 4
+#SBATCH --mem 100000
+#SBATCH -t 0-04
+#SBATCH --export=ALL
+
+. ~/.bashrc
+echo $TMPDIR
+user_name=$(whoami)
+cd ../../..
+conda activate ibl-mm
+python src/prepare_data.py --base_path /scratch/bdtg/${user_name}/datasets --n_sessions 200
+cd script/ppwang/delta
+
